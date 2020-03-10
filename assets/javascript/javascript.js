db9b821aa2735e0d50be46b15c52c28fe338aee2
Hung Le <hungle913@gmail.com>
2020-03-10 15:11:25 -0700

    Added ID to button to add trains. Also added firebase initialize code for javascript file.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
-    // jQuery methods go here...
+  var firebaseConfig = {
+    apiKey: "AIzaSyAARauqpKNNBNRWUd7b9kYHxvEpNPzeABA",
+    authDomain: "bootcamp-16a34.firebaseapp.com",
+    databaseURL: "https://bootcamp-16a34.firebaseio.com",
+    projectId: "bootcamp-16a34",
+    storageBucket: "bootcamp-16a34.appspot.com",
+    messagingSenderId: "923397960484",
+    appId: "1:923397960484:web:40699617e0275bd7bcb9b1",
+    measurementId: "G-XHSHJSZJ0J"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
   
-  });
+});
